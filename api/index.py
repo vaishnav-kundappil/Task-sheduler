893f9a6456000429a02038abfa8b52d3e75a28c6
vaishnav.p <vaishnav.p@innovaturelabs.com>
2025-12-16 10:11:14 +0530

    fd

--- api/index.py
+++ api/index.py
@@ -1,6 +1,9 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "credit_app.settings")
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "credit_app.settings"
+)
 
 app = get_wsgi_application()
