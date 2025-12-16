2374cbde7aa8c3b1a3c10ec6aba9628562f53521
vaishnav.p <vaishnav.p@innovaturelabs.com>
2025-12-16 10:05:44 +0530

    new

--- api/index.py
+++ api/index.py
@@ -3,4 +3,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "credit_app.settings")
 
-application = get_wsgi_application()
+app = get_wsgi_application()
